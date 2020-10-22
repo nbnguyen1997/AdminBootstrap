245af702bd18740bc6a8d748bd01be67ad5ded4a
nbnguyen1997 <hoctapbinhnguyen@gmail.com>
2020-10-22 21:02:54 +0700

    thay doi du lieu file objects.txt va render thanh cong

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
     $('#dataTable').DataTable({
-        "ajax": "./arrays.txt"
+
+        dom: "Bfrtip",
+        ajax: "../objects.txt",
+        order: [
+            [1, 'asc']
+        ],
+        columns: [
+
+            { data: "first_name" },
+            { data: "last_name" },
+            { data: "position" },
+            { data: "office" },
+            { data: "age" },
+            { data: "start_date" },
+            { data: "salary", render: $.fn.dataTable.render.number(',', '.', 0, '$') }
+        ],
+        select: {
+            style: 'os',
+            selector: 'td:first-child'
+        }
+        // },
+        // buttons: [
+        //     { extend: "create", editor: editor },
+        //     { extend: "edit", editor: editor },
+        //     { extend: "remove", editor: editor }
+        // ]
     });
 });
